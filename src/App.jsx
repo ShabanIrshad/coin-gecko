4a4d1bac8629755f472ea30b65d08125f75a64fa
Shaban Irshad <shabanirshad408@gmail.com>
2026-03-09 11:38:18 +0530

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import TableRow from "./TableRow";
 import "./App.css";
-import nodata from './assets/nodata.svg'
-import loadingImg from './assets/loading.gif';
+import nodata from "./assets/nodata.svg";
+import loadingImg from "./assets/loading.gif";
 
 const App = () => {
   const [data, setData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
-  const [loading,setLoading]=useState(true);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch(
@@ -23,7 +23,7 @@ const App = () => {
 
   useEffect(() => {
     setFilteredData(data);
-  }, [data])
+  }, [data]);
 
   const handleSearch = (event) => {
     const s = event.target.value;
@@ -46,55 +46,60 @@ const App = () => {
   return (
     <>
       <main className="ai-box">
-        {loading?<div className="loading-div">
-                      <img src={loadingImg} alt="loading_img" />
-                      <h2>Loading...</h2>
-                   </div>:
-        <div className="app-container">
-          <div className="title-div">
-            <h2 className="heading">Crypto Market📈</h2>
-            <div className="search-div">
-              <form onSubmit={(e) => handleSubmit(e)}>
-                <input
-                  type="text"
-                  onChange={(e) => handleSearch(e)}
-                  placeholder="Search Bitcoin..."
-                />
-              </form>
+        {loading ? (
+          <div className="loading-div">
+            <img src={loadingImg} alt="loading_img" />
+            <h2>Loading...</h2>
+          </div>
+        ) : (
+          <div className="app-container">
+            <div className="title-div">
+              <h2 className="heading">Crypto Market📈</h2>
+              <div className="search-div">
+                <form onSubmit={(e) => handleSubmit(e)}>
+                  <input
+                    type="text"
+                    onChange={(e) => handleSearch(e)}
+                    placeholder="Search Bitcoin..."
+                  />
+                </form>
+              </div>
+            </div>
+
+            <div className="table-container">
+              {filteredData.length > 0 ? (
+                <table>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Coin</th>
+                      <th>
+                        Price Change/24<sup>th</sup>
+                      </th>
+                      <th>
+                        low / high 24<sup>th</sup>
+                      </th>
+                      <th>Price (USD)</th>
+                      <th>M.Cap</th>
+                      <th>Volume</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {filteredData.length > 0 &&
+                      filteredData.map((data, index) => (
+                        <TableRow key={index} data={data} />
+                      ))}
+                  </tbody>
+                </table>
+              ) : (
+                <div className="nodata">
+                  <img src={nodata} alt="no_data_found" />
+                  <h1>No Data Found!</h1>
+                </div>
+              )}
             </div>
           </div>
-                    
-          <div className="table-container">
-            {filteredData.length>0?
-             <table>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Coin</th>
-                  <th>
-                    Price Change/24<sup>th</sup>
-                  </th>
-                  <th>
-                    low / high 24<sup>th</sup>
-                  </th>
-                  <th>Price (USD)</th>
-                  <th>M.Cap</th>
-                  <th>Volume</th>
-                </tr>
-              </thead>
-              <tbody>
-                {filteredData.length > 0 &&
-                  filteredData.map((data,index) => <TableRow key={index} data={data} />)}
-              </tbody>
-            </table>:<div className="nodata">
-                <img src={nodata} alt="no_data_found"/>
-                <h1>No Data Found!</h1>
-            </div>}
-          </div>
-          
-         
-        </div>
-        }
+        )}
       </main>
     </>
   );
